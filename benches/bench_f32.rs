c5ab5e7f3131148d1ccfed7a8feb1eff6660be22
minimalrust <minimalrustdev@gmail.com>
2020-01-19 18:39:54 +0000

    Refactor benchmarks into two files

--- benches/bench_f32.rs
+++ benches/bench_f32.rs
@@ -20,14 +20,22 @@ fn max_f32(c: &mut Criterion) {
     c.bench_function("argmax_f32", |b| {
         b.iter(|| argmm::argmax(black_box(data.as_slice())))
     });
-}
-
-fn simd_max_f32(c: &mut Criterion) {
     let data = get_array_f32();
     c.bench_function("argmax_simd f32", |b| {
         b.iter(|| argmm::argmax_f32(black_box(data.as_slice())))
     });
 }
 
-criterion_group!(benches, max_f32, simd_max_f32);
+fn min_f32(c: &mut Criterion) {
+    let data = get_array_f32();
+    c.bench_function("argmin_f32", |b| {
+        b.iter(|| argmm::argmin(black_box(data.as_slice())))
+    });
+    let data = get_array_f32();
+    c.bench_function("argmin_simd f32", |b| {
+        b.iter(|| argmm::argmin_f32(black_box(data.as_slice())))
+    });
+}
+
+criterion_group!(benches, max_f32, min_f32);
 criterion_main!(benches);
--- benches/bench_i32.rs
+++ benches/bench_i32.rs
@@ -15,19 +15,27 @@ fn get_array_i32() -> Vec<i32> {
     rng.sample_iter(uni).take(1024).collect()
 }
 
+fn min_i32(c: &mut Criterion) {
+    let data = get_array_i32();
+    c.bench_function("argmin_i32", |b| {
+        b.iter(|| argmm::argmin(black_box(data.as_slice())))
+    });
+    let data = get_array_i32();
+    c.bench_function("argmin_simd i32", |b| {
+        b.iter(|| argmm::argmin_i32(black_box(data.as_slice())))
+    });
+}
+
 fn max_i32(c: &mut Criterion) {
     let data = get_array_i32();
     c.bench_function("argmax_i32", |b| {
         b.iter(|| argmm::argmax(black_box(data.as_slice())))
     });
-}
-
-fn simd_max_i32(c: &mut Criterion) {
     let data = get_array_i32();
     c.bench_function("argmax_simd i32", |b| {
         b.iter(|| argmm::argmax_i32(black_box(data.as_slice())))
     });
 }
 
-criterion_group!(benches, max_i32, simd_max_i32);
+criterion_group!(benches, min_i32, max_i32);
 criterion_main!(benches);
