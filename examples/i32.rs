b778764e73e1a5a7073a6b536f7914fd25389335
minimalrust <minimalrustdev@gmail.com>
2020-01-29 06:35:02 +0000

    Fix the broken example

--- examples/i32.rs
+++ examples/i32.rs
@@ -4,7 +4,7 @@ use rand_distr::Uniform;
 
 fn get_array_i32() -> Vec<i32> {
     let rng = thread_rng();
-    let uni = Uniform::from(0..100_000).unwrap();
+    let uni = Uniform::from(0..100_000);
     rng.sample_iter(uni).take(512).collect()
 }
 
