ffed06ecf0d366beb4dea6da227cd10816fcfc4d
minimalrust <minimalrustdev@gmail.com>
2020-02-04 22:47:23 +0000

    Change iteration to return chunks

--- src/simd.rs
+++ src/simd.rs
@@ -30,10 +30,10 @@ unsafe fn core_argmin(sim_arr: &[f32], rem_offset: usize) -> (f32, usize) {
 
     let mut values_low = _mm_loadu_ps(sim_arr.get_unchecked(0));
 
-    sim_arr.iter().step_by(4).skip(1).for_each(|step| {
+    sim_arr.chunks(4).skip(1).for_each(|step| {
         new_index_low = _mm_add_ps(new_index_low, increment);
 
-        let new_values = _mm_loadu_ps(step as *const _);
+        let new_values = _mm_loadu_ps(&step[0] as *const _);
         let lt_mask = _mm_cmplt_ps(new_values, values_low);
 
         values_low = _mm_min_ps(new_values, values_low);
@@ -98,10 +98,10 @@ unsafe fn core_argmax(sim_arr: &[f32], rem_offset: usize) -> (f32, usize) {
 
     let mut values_high = _mm_loadu_ps(sim_arr.get_unchecked(0));
 
-    sim_arr.iter().step_by(4).skip(1).for_each(|step| {
+    sim_arr.chunks(4).skip(1).for_each(|step| {
         new_index_high = _mm_add_ps(new_index_high, increment);
 
-        let new_values = _mm_loadu_ps(step as *const _);
+        let new_values = _mm_loadu_ps(&step[0] as *const _);
         let gt_mask = _mm_cmpgt_ps(new_values, values_high);
 
         values_high = _mm_max_ps(new_values, values_high);
