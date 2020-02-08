3f351c030079f0ea5fdd3271e3fd8ec2e4fd1c8f
minimalrust <minimalrustdev@gmail.com>
2020-02-08 06:48:18 +0000

    Simplify examples

--- examples/f32.rs
+++ examples/f32.rs
@@ -1,16 +1,9 @@
 use argmm::ArgMinMax;
-use rand::{thread_rng, Rng};
-use rand_distr::Exp;
-
-fn get_array_f32() -> Vec<f32> {
-    let rng = thread_rng();
-    let exp = Exp::new(1.0).unwrap();
-    rng.sample_iter(exp).take(512).collect()
-}
 
 fn main() {
-    let data = get_array_f32();
-    let res = data.argmin();
-    let res2 = data.argmax();
-    println!("{:?} / {:?}", res, res2);
+    let data = vec![1., 7., -4., 13., 20., -4., -8., 0., 15., 2.];
+    let min_index = data.argmin();
+    let max_index = data.argmax();
+    assert_eq!(min_index, Some(6));
+    assert_eq!(max_index, Some(4));
 }
--- examples/i32.rs
+++ examples/i32.rs
@@ -1,16 +1,9 @@
 use argmm::ArgMinMax;
-use rand::{thread_rng, Rng};
-use rand_distr::Uniform;
-
-fn get_array_i32() -> Vec<i32> {
-    let rng = thread_rng();
-    let uni = Uniform::from(0..1000);
-    rng.sample_iter(uni).take(512).collect()
-}
 
 fn main() {
-    let data = get_array_i32();
-    let res = data.argmin();
-    let res2 = data.argmax();
-    println!("{:?} / {:?}", res, res2);
+    let data = vec![-4, -8, 0, 15, 2, 1, 7, -4, 13, 20];
+    let min_index = data.argmin();
+    let max_index = data.argmax();
+    assert_eq!(min_index, Some(1));
+    assert_eq!(max_index, Some(9));
 }
