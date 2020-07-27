f649eb5087eb8d1b2bf0bfb411d6ec24096ac68d
minimalrust <minimalrustdev@gmail.com>
2020-07-27 22:06:33 +0100

    Remove examples directory

--- examples/f32.rs
+++
@@ -1,9 +0,0 @@
-use argmm::ArgMinMax;
-
-fn main() {
-    let data = vec![1f32, 7., -4., 13., 20., -4., -8., 0., 15., 2.];
-    let min_index = data.argmin().unwrap();
-    let max_index = data.argmax().unwrap();
-    assert_eq!(data[min_index], -8.);
-    assert_eq!(data[max_index], 20.);
-}
--- examples/i32.rs
+++
@@ -1,9 +0,0 @@
-use argmm::ArgMinMax;
-
-fn main() {
-    let data = vec![-4i32, -8, 0, 15, 2, 1, 7, -4, 13, 20];
-    let min_index = data.argmin().unwrap();
-    let max_index = data.argmax().unwrap();
-    assert_eq!(data[min_index], -8);
-    assert_eq!(data[max_index], 20);
-}
