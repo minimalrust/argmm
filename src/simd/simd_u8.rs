f8e08e5cdbf36530e01bc35788b35a8d5b593ccd
minimalrust <minimalrustdev@gmail.com>
2020-07-30 00:48:09 +0100

    Use 16bit vector for 8 element operations where possible

--- src/simd/simd_u8.rs
+++ src/simd/simd_u8.rs
@@ -1,24 +1,110 @@
 use crate::generic::{simple_argmax, simple_argmin};
 use crate::task::split_array;
 use std::arch::x86_64::*;
+use std::cmp::Ordering;
 
 pub fn argmin_u8(arr: &[u8]) -> Option<usize> {
-    match split_array(arr, 4) {
+    let n = arr.len();
+    let mut simd_func: unsafe fn(&[u8], usize) -> (u8, usize) = core_argmin;
+    let mut mod_size = 4;
+
+    if (17..std::i16::MAX).contains(&(n as i16)) {
+        simd_func = core_argmin_ext;
+        mod_size = 8;
+    };
+
+    match split_array(arr, mod_size) {
         (Some(rem), Some(sim)) => {
             let rem_min_index = simple_argmin(rem);
             let rem_result = (rem[rem_min_index], rem_min_index);
-            let sim_result = unsafe { core_argmin(sim, rem.len()) };
-            let final_test = &[rem_result, sim_result];
-            let final_index = simple_argmin(final_test);
-            Some(final_test[final_index].1)
+            let sim_result = unsafe { simd_func(sim, rem.len()) };
+            let final_index = match rem_result.0.cmp(&sim_result.0) {
+                Ordering::Less => rem_result.1,
+                Ordering::Equal => std::cmp::min(rem_result.1, sim_result.1),
+                Ordering::Greater => sim_result.1,
+            };
+            Some(final_index)
         }
         (Some(rem), None) => Some(simple_argmin(rem)),
         (None, Some(sim)) => {
-            let sim_result = unsafe { core_argmin(sim, 0) };
+            let sim_result = unsafe { simd_func(sim, 0) };
             Some(sim_result.1)
         }
         (None, None) => None,
     }
+}
+
+unsafe fn core_argmin_ext(sim_arr: &[u8], rem_offset: usize) -> (u8, usize) {
+    let offset = _mm_set1_epi16(rem_offset as i16);
+    let mut index_low = _mm_add_epi16(_mm_set_epi16(7, 6, 5, 4, 3, 2, 1, 0), offset);
+
+    let increment = _mm_set1_epi16(8);
+    let mut new_index_low = index_low;
+
+    let mut values_low = _mm_set_epi16(
+        sim_arr[7] as i16,
+        sim_arr[6] as i16,
+        sim_arr[5] as i16,
+        sim_arr[4] as i16,
+        sim_arr[3] as i16,
+        sim_arr[2] as i16,
+        sim_arr[1] as i16,
+        sim_arr[0] as i16,
+    );
+
+    sim_arr.chunks_exact(8).skip(1).for_each(|step| {
+        new_index_low = _mm_add_epi16(new_index_low, increment);
+
+        let new_values = _mm_set_epi16(
+            step[7] as i16,
+            step[6] as i16,
+            step[5] as i16,
+            step[4] as i16,
+            step[3] as i16,
+            step[2] as i16,
+            step[1] as i16,
+            step[0] as i16,
+        );
+
+        let lt_mask = _mm_cmplt_epi16(new_values, values_low);
+
+        values_low = _mm_or_si128(
+            _mm_and_si128(new_values, lt_mask),
+            _mm_andnot_si128(lt_mask, values_low),
+        );
+        index_low = _mm_or_si128(
+            _mm_and_si128(new_index_low, lt_mask),
+            _mm_andnot_si128(lt_mask, index_low),
+        );
+    });
+
+    let highpack = _mm_unpackhi_epi16(values_low, values_low);
+    let lowpack = _mm_unpacklo_epi16(values_low, values_low);
+    let lowest = _mm_min_epi16(highpack, lowpack);
+
+    let highpack = _mm_unpackhi_epi16(lowest, lowest);
+    let lowpack = _mm_unpacklo_epi16(lowest, lowest);
+    let lowest = _mm_min_epi16(highpack, lowpack);
+
+    let highpack = _mm_unpackhi_epi16(lowest, lowest);
+    let lowpack = _mm_unpacklo_epi16(lowest, lowest);
+    let lowest = _mm_min_epi16(highpack, lowpack);
+
+    let low_mask = _mm_cmpeq_epi16(lowest, values_low);
+
+    index_low = _mm_or_si128(
+        _mm_and_si128(index_low, low_mask),
+        _mm_andnot_si128(low_mask, _mm_set1_epi16(std::i16::MAX)),
+    );
+
+    let value_array = std::mem::transmute::<__m128i, [i16; 8]>(values_low);
+    let index_array = std::mem::transmute::<__m128i, [i16; 8]>(index_low);
+
+    let min_index = simple_argmin(&index_array);
+    let value = *value_array.get_unchecked(min_index);
+    let index = *index_array.get_unchecked(min_index);
+
+    (value as u8, index as usize)
 }
 
 unsafe fn core_argmin(sim_arr: &[u8], rem_offset: usize) -> (u8, usize) {
@@ -59,13 +145,11 @@ unsafe fn core_argmin(sim_arr: &[u8], rem_offset: usize) -> (u8, usize) {
 
     let highpack = _mm_unpackhi_epi32(values_low, values_low);
     let lowpack = _mm_unpacklo_epi32(values_low, values_low);
+    let lowest = _mm_min_epi32(highpack, lowpack);
 
-    let mut lowest = _mm_min_epi32(highpack, lowpack);
-
-    let highestpack = _mm_unpackhi_epi32(lowest, lowest);
-    let lowestpack = _mm_unpacklo_epi32(lowest, lowest);
-
-    lowest = _mm_min_epi32(highestpack, lowestpack);
+    let highpack = _mm_unpackhi_epi32(lowest, lowest);
+    let lowpack = _mm_unpacklo_epi32(lowest, lowest);
+    let lowest = _mm_min_epi32(highpack, lowpack);
 
     let low_mask = _mm_cmpeq_epi32(lowest, values_low);
 
@@ -85,18 +169,30 @@ unsafe fn core_argmin(sim_arr: &[u8], rem_offset: usize) -> (u8, usize) {
 }
 
 pub fn argmax_u8(arr: &[u8]) -> Option<usize> {
-    match split_array(arr, 4) {
+    let n = arr.len();
+    let mut simd_func: unsafe fn(&[u8], usize) -> (u8, usize) = core_argmax;
+    let mut mod_size = 4;
+
+    if (17..=std::i16::MAX).contains(&(n as i16)) {
+        simd_func = core_argmax_ext;
+        mod_size = 8;
+    };
+
+    match split_array(arr, mod_size) {
         (Some(rem), Some(sim)) => {
             let rem_min_index = simple_argmax(rem);
             let rem_result = (rem[rem_min_index], rem_min_index);
-            let sim_result = unsafe { core_argmax(sim, rem.len()) };
-            let final_test = &[rem_result, sim_result];
-            let final_index = simple_argmax(final_test);
-            Some(final_test[final_index].1)
+            let sim_result = unsafe { simd_func(sim, rem.len()) };
+            let final_index = match sim_result.0.cmp(&rem_result.0) {
+                Ordering::Less => rem_result.1,
+                Ordering::Equal => std::cmp::min(rem_result.1, sim_result.1),
+                Ordering::Greater => sim_result.1,
+            };
+            Some(final_index)
         }
         (Some(rem), None) => Some(simple_argmax(rem)),
         (None, Some(sim)) => {
-            let sim_result = unsafe { core_argmax(sim, 0) };
+            let sim_result = unsafe { simd_func(sim, 0) };
             Some(sim_result.1)
         }
         (None, None) => None,
@@ -106,6 +202,7 @@ pub fn argmax_u8(arr: &[u8]) -> Option<usize> {
 unsafe fn core_argmax(sim_arr: &[u8], rem_offset: usize) -> (u8, usize) {
     let offset = _mm_set1_epi32(rem_offset as i32);
     let mut index_high = _mm_add_epi32(_mm_set_epi32(3, 2, 1, 0), offset);
+
     let mut new_index_high = index_high;
 
     let increment = _mm_set1_epi32(4);
@@ -141,13 +238,11 @@ unsafe fn core_argmax(sim_arr: &[u8], rem_offset: usize) -> (u8, usize) {
 
     let highpack = _mm_unpackhi_epi32(values_high, values_high);
     let lowpack = _mm_unpacklo_epi32(values_high, values_high);
+    let highest = _mm_max_epi32(highpack, lowpack);
 
-    let mut highest = _mm_max_epi32(highpack, lowpack);
-
-    let highestpack = _mm_unpackhi_epi32(highest, highest);
-    let lowestpack = _mm_unpacklo_epi32(highest, highest);
-
-    highest = _mm_max_epi32(highestpack, lowestpack);
+    let highpack = _mm_unpackhi_epi32(highest, highest);
+    let lowpack = _mm_unpacklo_epi32(highest, highest);
+    let highest = _mm_max_epi32(highpack, lowpack);
 
     let high_mask = _mm_cmpeq_epi32(highest, values_high);
 
@@ -158,6 +253,79 @@ unsafe fn core_argmax(sim_arr: &[u8], rem_offset: usize) -> (u8, usize) {
 
     let value_array = std::mem::transmute::<__m128i, [u32; 4]>(values_high);
     let index_array = std::mem::transmute::<__m128i, [i32; 4]>(index_high);
+
+    let min_index = simple_argmin(&index_array);
+    let value = *value_array.get_unchecked(min_index);
+    let index = *index_array.get_unchecked(min_index);
+
+    (value as u8, index as usize)
+}
+
+unsafe fn core_argmax_ext(sim_arr: &[u8], rem_offset: usize) -> (u8, usize) {
+    let offset = _mm_set1_epi16(rem_offset as i16);
+    let mut index_high = _mm_add_epi16(_mm_set_epi16(7, 6, 5, 4, 3, 2, 1, 0), offset);
+    let mut new_index_high = index_high;
+
+    let increment = _mm_set1_epi16(8);
+
+    let mut values_high = _mm_set_epi16(
+        sim_arr[7] as i16,
+        sim_arr[6] as i16,
+        sim_arr[5] as i16,
+        sim_arr[4] as i16,
+        sim_arr[3] as i16,
+        sim_arr[2] as i16,
+        sim_arr[1] as i16,
+        sim_arr[0] as i16,
+    );
+
+    sim_arr.chunks_exact(8).skip(1).for_each(|step| {
+        new_index_high = _mm_add_epi16(new_index_high, increment);
+
+        let new_values = _mm_set_epi16(
+            step[7] as i16,
+            step[6] as i16,
+            step[5] as i16,
+            step[4] as i16,
+            step[3] as i16,
+            step[2] as i16,
+            step[1] as i16,
+            step[0] as i16,
+        );
+
+        let gt_mask = _mm_cmpgt_epi16(new_values, values_high);
+
+        values_high = _mm_or_si128(
+            _mm_and_si128(new_values, gt_mask),
+            _mm_andnot_si128(gt_mask, values_high),
+        );
+        index_high = _mm_or_si128(
+            _mm_and_si128(new_index_high, gt_mask),
+            _mm_andnot_si128(gt_mask, index_high),
+        );
+    });
+
+    let highpack = _mm_unpackhi_epi16(values_high, values_high);
+    let lowpack = _mm_unpacklo_epi16(values_high, values_high);
+    let highest = _mm_max_epi16(highpack, lowpack);
+
+    let highpack = _mm_unpackhi_epi16(highest, highest);
+    let lowpack = _mm_unpacklo_epi16(highest, highest);
+    let highest = _mm_max_epi16(highpack, lowpack);
+
+    let highpack = _mm_unpackhi_epi16(highest, highest);
+    let lowpack = _mm_unpacklo_epi16(highest, highest);
+    let highest = _mm_max_epi16(highpack, lowpack);
+
+    let high_mask = _mm_cmpeq_epi16(highest, values_high);
+
+    index_high = _mm_or_si128(
+        _mm_and_si128(index_high, high_mask),
+        _mm_andnot_si128(high_mask, _mm_set1_epi16(std::i16::MAX)),
+    );
+
+    let value_array = std::mem::transmute::<__m128i, [i16; 8]>(values_high);
+    let index_array = std::mem::transmute::<__m128i, [i16; 8]>(index_high);
 
     let min_index = simple_argmin(&index_array);
     let value = *value_array.get_unchecked(min_index);
@@ -179,9 +347,23 @@ mod tests {
     }
 
     #[test]
-    fn test_both_versions_return_the_same_results() {
-        let data = get_array_u8(1025);
+    fn test_both_versions_return_the_same_results_short_array() {
+        let data = get_array_u8(9);
         assert_eq!(data.len() % 4, 1);
+
+        let min_index = argmin_u8(&data).unwrap();
+        let max_index = argmax_u8(&data).unwrap();
+        let argmin_index = simple_argmin(&data);
+        let argmax_index = simple_argmax(&data);
+
+        assert_eq!(argmin_index, min_index);
+        assert_eq!(argmax_index, max_index);
+    }
+
+    #[test]
+    fn test_both_versions_return_the_same_results_long_array() {
+        let data = get_array_u8(1025);
+        assert_eq!(data.len() % 8, 1);
 
         let min_index = argmin_u8(&data).unwrap();
         let max_index = argmax_u8(&data).unwrap();
